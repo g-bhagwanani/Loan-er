8b12efb92357e19b561ba10755305d4c58e9e651
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2019-06-23 13:21:45 +0530

    added otpion to change mob no and resend otp

--- hello.py
+++ hello.py
@@ -1,13 +1,57 @@
 from flask import Flask, redirect, url_for, request ,render_template
+import urllib.request
+import urllib.parse
+import random
+
 app = Flask(__name__)
 
-otp = '4151'
+otp = '0'
 number = '0'
 
+url = "https://www.fast2sms.com/dev/bulk"
+
+headers = {
+    'cache-control': "no-cache"
+}
+
+#method to send otp
+@app.route('/resend-otp')
+def sendSMS():
+    apikey = 'pIyBJ6pdYX8-bGlGy8HXMOL0FG6RGYRo4jZ6W1A0Qf'
+    numbers = '91' + number
+    global otp
+    otp = str(random.randint(1000, 9999))
+    #send otp to the Number
+    data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
+        'message' : "Your ABFL OTP is " + otp})
+    data = data.encode('utf-8')
+    request = urllib.request.Request("https://api.textlocal.in/send/?")
+    f = urllib.request.urlopen(request, data)
+    fr = f.read()
+    #console.log(fr)
+
+    return redirect(url_for('get_otp'))
+
+#login page on startup
 @app.route('/')
 def login():
     return render_template('login.html')
 
+#to trigger the sending of otp to the mobile number typed by the user
+@app.route('/login', methods = ['GET', 'POST'])
+def send_otp():
+    global number
+    if request.method == 'POST':
+        number = request.form['num']
+    sendSMS()
+    return redirect(url_for('get_otp'))
+
+#to direct user to enter the otp sent to this phone
+@app.route('/enter-otp')
+def get_otp():
+    return render_template('verify.html')
+
+#to verify the otp typed in by the user
 @app.route('/verify', methods = ['GET', 'POST'])
 def verify_otp():
     if request.method == 'POST':
@@ -20,18 +64,6 @@ def verify_otp():
             return 'Successfully registered with number %s' % number
         else:
             return 'Incorrect otp'
-
-@app.route('/enter-otp')
-def get_otp():
-    return render_template('verify.html')
-
-@app.route('/login', methods = ['GET', 'POST'])
-def send_otp():
-    global number
-    if request.method == 'POST':
-        number = request.form['num']
-        #send otp to the Number
-        return redirect(url_for('get_otp'))
 
 if __name__ == '__main__':
     app.run(debug = True)
--- login.py
+++ login.py
@@ -1,11 +1,15 @@
-import requests
+import urllib.request
+import urllib.parse
 
-url = "https://www.fast2sms.com/dev/wallet"
+def sendSMS(apikey, numbers, message):
+    data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
+        'message' : message})
+    data = data.encode('utf-8')
+    request = urllib.request.Request("https://api.textlocal.in/send/?")
+    f = urllib.request.urlopen(request, data)
+    fr = f.read()
+    return(fr)
 
-headers = {
-    'authorization': "mqgKX7nlZpIvJPdhMF42tr5bz1RTBe8VEiwoH9CWUAYQSG6kOc0ubQ3Hd8z5xIkpGrnvlgwj1DoEJKX9",
-    }
-
-response = requests.request("POST", url, headers=headers)
-
-print(response.text)
+#resp =  sendSMS('pIyBJ6pdYX8-bGlGy8HXMOL0FG6RGYRo4jZ6W1A0Qf', '919284621757',
+    #'This is your message')
+print (resp)

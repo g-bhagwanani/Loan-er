497b76758dc8a6b41c344d9b9dab4420a17ddf30
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2019-07-12 05:07:09 +0530

    Frontend completed

--- static/js/chatpage.js
+++ static/js/chatpage.js
@@ -2,12 +2,18 @@ var botui = new BotUI('my-botui-app');
 
 var i = 0;
 
-$(document).ready(function(){
-  $(".yes-no, .temp").hide();
+var amount;
 
+$(document).ready(function(){
+  $(".yes-no, .temp, #cam-hidden, #gallery-hidden, .cam-gallery, .amt-ip").hide();
+
+  amount = 45000;
+  $("#emi")[0].innerHTML = 'EMI: &#8377 ' + calc_emi(amount, 18);
   //trial
   bot_txt('hey');
   //accept_or_deny();
+  //cam_or_gallery();
+  duration();
 });
 
 //bot messages
@@ -19,22 +25,62 @@ function bot_txt(msg){
   });
 }
 
+function calc_emi(amt, dur){
+  interest = dur - 12 + 10;
+  return Math.ceil(amt*(1 + interest/100)/dur);
+}
+
 function accept_or_deny(){
   $(".yes-no").show();
-  $('.footer').hide();
+  $('.send-box').hide();
 }
 
 $("#yes").click(function(){
   human_txt('yes');
   $(".yes-no").hide();
-  $('.footer').show();
+  $('.send-box').show();
 });
 
 $("#no").click(function(){
   human_txt('no');
   $(".yes-no").hide();
-  $('.footer').show();
+  $('.send-box').show();
 });
+
+function cam_or_gallery(){
+  $(".cam-gallery").show();
+  $('.send-box').hide();
+}
+
+function cam_send(){
+  fileObj = $('#cam-hidden')[0].files[0];
+  const objectURL = window.URL.createObjectURL(fileObj);
+  console.log(objectURL);
+  $('.cam-gallery').hide();
+  $('.send-box').show();
+  human_img(objectURL);
+}
+
+function gallery_send(){
+  fileObj = $('#gallery-hidden')[0].files[0];
+  const objectURL = window.URL.createObjectURL(fileObj);
+  console.log(objectURL);
+  $('.cam-gallery').hide();
+  $('.send-box').show();
+  human_img(objectURL);
+}
+
+function duration(){
+  $(".amt-ip").show();
+  $('.send-box').hide();
+}
+
+function send_duration(){
+  var dur = $("#myRange")[0].value;
+  human_txt(dur + ' months');
+  $('.amt-ip').hide();
+  $('.send-box').show();
+}
 
 //function to send msg from text-box
 $("#send-button").click(function(){
@@ -48,6 +94,13 @@ function human_txt(msg){
     content: msg
   });
   //insert logic to send msg to dialogflow
+}
+
+function human_img(src){
+  botui.message.human({
+    type: 'embed',
+    content: src
+});
 }
 
 function progress(){
@@ -68,6 +121,9 @@ function progress(){
   }
 }
 
-$(".amount-ip").on('input', function(){
-  $("#val")[0].innerHTML = this.value;
+$("#myRange").on('input', function(){
+  dur  =this.value;
+  let temp = amount;
+  $("#emi")[0].innerHTML = 'EMI: &#8377 ' + calc_emi(temp, dur);
+  $("#dur")[0].innerHTML = 'Duration: ' + dur + ' months';
 });

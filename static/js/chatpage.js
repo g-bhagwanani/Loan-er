87f0a680c2c29806ad96093bad35ec7fd1cf09f1
KaustubhDamania <kaustubh.damania@gmail.com>
2019-07-12 09:16:50 +0530

    Integration frontend+backend WIP

--- static/js/chatpage.js
+++ static/js/chatpage.js
@@ -2,10 +2,13 @@ var botui = new BotUI('my-botui-app');
 
 var i = 0;
 
+var intents = ['loan','get name', 'amount-1','loan period','email','pan','PAN pic upload', 'Aadhar number', 'Aadhar pic front','Aadhar pic back','Loan approved - yes','Bank details']
+var ptr = 0;
+
 var amount;
 
 $(document).ready(function(){
-  $(".yes-no, .temp, #cam-hidden, #gallery-hidden, .cam-gallery, .amt-ip").hide();
+  $(".yes-no, .temp, #cam-hidden, #gallery-hidden, .cam-gallery, .amt-ip, .bank-ip").hide();
 
   amount = 45000;
   $("#emi")[0].innerHTML = 'EMI: &#8377 ' + calc_emi(amount, 18);
@@ -13,7 +16,7 @@ $(document).ready(function(){
   bot_txt('hey');
   //accept_or_deny();
   //cam_or_gallery();
-  duration();
+  // duration();
 });
 
 //bot messages
@@ -82,6 +85,20 @@ function send_duration(){
   $('.send-box').show();
 }
 
+function get_bank(){
+  $(".bank-ip").show();
+  $('.send-box').hide();
+}
+
+function send_bank(){
+  var acc = $("#acc-no")[0].value;
+  var ifsc = $("#ifsc")[0].value;
+  human_txt('Account No.: ' + acc);
+  human_txt('IFSC Code: ' + ifsc);
+  $('.bank-ip').hide();
+  $('.send-box').show();
+}
+
 //function to send msg from text-box
 $("#send-button").click(function(){
   var msg = $("#text-ip").val();
@@ -94,6 +111,52 @@ function human_txt(msg){
     content: msg
   });
   //insert logic to send msg to dialogflow
+    var url = window.location.href;
+    url = url.slice(0,url.length-5)+'/myapi';
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var response = JSON.parse(this.responseText);
+            messages = (response.fulfillmentMessages);
+            var intent_name = response.intent.displayName;
+            console.log(intent_name);
+            console.log(messages);
+            for (var response in messages) {
+                response = messages[response].text.text;
+                bot_txt(response[0]);
+            }
+            if(intent_name != 'Default Fallback Intent'){
+                ptr += 1;
+                if(ptr<intents.length)
+                    nextIntent = intents[ptr];
+                switch (nextIntent) {
+                    case 'loan period':
+                        //db call for amount
+                        duration();
+                        break;
+                    case 'PAN pic upload':
+                    case 'Aadhar pic front':
+                    case 'Aadhar pic back':
+                        cam_or_gallery();
+                        break;
+                    case 'Loan approved - yes':
+                        accept_or_deny();
+                        break;
+                    case 'Bank details':
+                        get_bank();
+                        break;
+                }
+            }
+        }
+    };
+    xhttp.open("POST", url, true);
+    xhttp.send(msg);
+    console.log('Some shit');
+
+
 }
 
 function human_img(src){

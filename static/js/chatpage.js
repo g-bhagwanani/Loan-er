78260219e4c2ec61fa279ec3ca5d3d653be3dca9
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2019-07-13 00:12:59 +0530

    progress bar completed

--- static/js/chatpage.js
+++ static/js/chatpage.js
@@ -2,10 +2,13 @@ var botui = new BotUI('my-botui-app');
 
 var i = 0;
 
+var intents = ['loan','get name', 'amount-1','loan period','email','pan','PAN pic upload', 'Aadhar number', 'Aadhar pic front','Aadhar pic back','Loan approved - yes','Bank details']
+var ptr = 0;
+
 var amount;
 
 $(document).ready(function(){
-  $(".yes-no, .temp, #cam-hidden, #gallery-hidden, .cam-gallery, .amt-ip").hide();
+  $(".yes-no, .temp, #cam-hidden, #gallery-hidden, .cam-gallery, .amt-ip, .bank-ip").hide();
 
   amount = 45000;
   $("#emi")[0].innerHTML = 'EMI: &#8377 ' + calc_emi(amount, 18);
@@ -13,7 +16,7 @@ $(document).ready(function(){
   bot_txt('hey');
   //accept_or_deny();
   //cam_or_gallery();
-  duration();
+  // duration();
 });
 
 //bot messages
@@ -56,18 +59,20 @@ function cam_send(){
   fileObj = $('#cam-hidden')[0].files[0];
   const objectURL = window.URL.createObjectURL(fileObj);
   console.log(objectURL);
+
   $('.cam-gallery').hide();
   $('.send-box').show();
-  human_img(objectURL);
+  human_img(objectURL, fileObj.toString());
 }
 
 function gallery_send(){
   fileObj = $('#gallery-hidden')[0].files[0];
   const objectURL = window.URL.createObjectURL(fileObj);
   console.log(objectURL);
+
   $('.cam-gallery').hide();
   $('.send-box').show();
-  human_img(objectURL);
+  human_img(objectURL, fileObj);
 }
 
 function duration(){
@@ -79,6 +84,21 @@ function send_duration(){
   var dur = $("#myRange")[0].value;
   human_txt(dur + ' months');
   $('.amt-ip').hide();
+  $('.send-box').show();
+}
+
+function get_bank(){
+  $(".bank-ip").show();
+  $('.send-box').hide();
+}
+
+function send_bank(){
+  var acc = $("#acc-no")[0].value;
+  var ifsc = $("#ifsc")[0].value;
+  var name = $("#name")[0].value;
+  human_txt('Account No.: ' + acc);
+  human_txt('IFSC Code: ' + ifsc + '\n' + 'Account holder\'s name: ' + name);
+  $('.bank-ip').hide();
   $('.send-box').show();
 }
 
@@ -94,13 +114,78 @@ function human_txt(msg){
     content: msg
   });
   //insert logic to send msg to dialogflow
+  send_to_server(msg);
+
 }
 
-function human_img(src){
+function human_img(src, obj){
   botui.message.human({
     type: 'embed',
     content: src
-});
+  });
+  send_to_server('xyz', true);
+}
+
+function send_to_server(msg, is_img=false){
+    var url = window.location.href;
+    url = url.slice(0,url.length-5)+'/myapi';
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var response = JSON.parse(this.responseText);
+            messages = (response.fulfillmentMessages);
+            var intent_name = response.intent.displayName;
+            console.log(intent_name);
+            console.log(messages);
+            for (var response in messages) {
+                response = messages[response].text.text;
+                bot_txt(response[0]);
+            }
+            if(intent_name != 'Default Fallback Intent'){
+                ptr += 1;
+                if(ptr<intents.length) {
+                    nextIntent = intents[ptr];
+                    switch (nextIntent) {
+                        case 'email':
+                            console.log('25% completed :))');
+                            progress();
+                            break;
+                        case 'loan period':
+                            //db call for amount
+                            duration();
+                            break;
+                        case 'Aadhar number':
+                            progress();
+                            break;
+                        case 'PAN pic upload':
+                        case 'Aadhar pic front':
+                        case 'Aadhar pic back':
+                            cam_or_gallery();
+                            break;
+                        case 'Loan approved - yes':
+                            progress();
+                            accept_or_deny();
+                            break;
+                        case 'Bank details':
+                            get_bank();
+                            break;
+                    }
+                }
+                else {
+                    progress();
+                }
+            }
+        }
+    };
+    xhttp.open("POST", url, true);
+    if(is_img)
+        xhttp.setRequestHeader('file_type','image')
+    xhttp.send(msg);
+
+    console.log('Some shit');
 }
 
 function progress(){
